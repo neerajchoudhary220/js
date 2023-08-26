a507245c5e0193eec828f1b471c068769c0245e5
neeraj@pairoxz <neeraj@pairroxz.com>
2023-08-26 21:39:42 +0530

    add index and test file

--- file/test.js
+++ file/test.js
@@ -1,5 +1,32 @@
 $(document).ready(function () {
-    $("#mybtn").click(function () {
-        alert("working");
+
+    $(".copybtn").click(function () {
+        var txt = $(this).parent().find(".copyarea").text();
+        CopyToClipboard(txt);
+        var copied = $(this).parent().find(".badge")
+
+        var self = $(this);
+        self.removeClass('text-secondary');
+        self.addClass('text-success');
+
+        copied.fadeIn("slow", () => {
+            self.removeClass('text-success');
+        });
+        copied.fadeOut("slow",()=>{
+            self.addClass('text-secondary')
+        });
+
+
     });
+
+    // $(this).tooltip();
+
+
+    function CopyToClipboard(txt) {
+        var $temp = $("<input>")
+        $("body").append($temp);
+        $temp.val(txt).select();
+        document.execCommand("copy");
+        $temp.remove();
+    }
 })
