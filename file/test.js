9c965db1f7e56756f41a9a16ffa6d761fdebe29b
neeraj@pairoxz <neeraj@pairroxz.com>
2023-08-27 16:50:53 +0530

    updated removejscommet

--- file/test.js
+++ file/test.js
@@ -33,3 +33,22 @@ function CopyToClipboard(txt) {
 function RemoveHtmlTags(html) {
     return html.replace(/<br(.|)?>/gm, "neeraj").replace(/<[^>]*>/gm, "").replace(/neeraj/gm, "\n").replace(/&nbsp;/gm, " ");
 }
+
+    
+
+     function AnimateToolCopyBtn(){
+        var copyBtn = $('.toolCopyBtn');
+        copyBtn.fadeIn("slow",()=>{
+            copyBtn.text("Copied!");
+        })
+    
+        copyBtn.fadeOut("slow",()=>{
+            copyBtn.html(`Copy <i class="fa fa-copy"></i>`);
+        })
+    
+        copyBtn.fadeIn();
+    
+    
+     }
+
+
--- tools/removejscomment.js
+++ tools/removejscomment.js
@@ -1,9 +1,15 @@
-$(document).ready(function(){
-$("#removejscommentbtn").click(function(){
-    $("#jscode").val(removejscomment($("#jscode").val()));
+$(document).ready(function () {
+    $("#removejscommentbtn").click(function () {
+        $("#jscode").val(removejscomment($("#jscode").val()));
+    })
 })
- })
 
- function removejscomment(txt){
-    return txt.replace(/\/\/((.|\n)*?.*)/gm,"")
- }
+function removejscomment(txt) {
+    return txt.replace(/\/\/((.|\n)*?.*)/gm, "")
+}
+
+$(".toolCopyBtn").click(function(){
+    var txt =  $("#jscode").val();
+    CopyToClipboard(txt);
+    AnimateToolCopyBtn();
+ })
